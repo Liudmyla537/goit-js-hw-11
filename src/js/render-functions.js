6481e35b65eed48be9847cafd0d43fb3662c7644
Liudmyla537 <stomaludmila537@gmail.com>
2025-06-10 10:46:35 +0300

    add

---
+++ src/js/render-functions.js
@@ -0,0 +1,50 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryEl = document.querySelector('.gallery');
+const loaderEl = document.querySelector('.loader');
+
+const lightbox = new SimpleLightbox('.gallery a');
+
+export function createGallery(images) {
+  const markup = images
+    .map(
+      img => `
+    <li class="gallery-item">
+      <a href="${img.largeImageURL}">
+        <img src="${img.webformatURL}" data-src="${img.largeImageURL}" alt="${img.tags}" />
+      </a>
+      <div class="info">
+        <p>Likes</b>
+            ${img.likes}
+         </p>
+        <p>Views</b>
+            ${img.views}
+         </p>
+        <p>Comments</b>
+            ${img.comments}
+         </p>
+        <p>Downloads</b>
+            ${img.downloads}
+         </p>
+      </div>
+    </li>
+  `
+    )
+    .join('');
+
+  galleryEl.insertAdjacentHTML('beforeend', markup);
+  lightbox.refresh();
+}
+
+export function clearGallery() {
+  galleryEl.innerHTML = '';
+}
+
+export function showLoader() {
+  loaderEl.classList.remove('hidden');
+}
+
+export function hideLoader() {
+  loaderEl.classList.add('hidden');
+}
