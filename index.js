c9895dece82678f5a84a128ca20f6ee1a7afe7a0
Liudmyla537 <stomaludmila537@gmail.com>
2025-06-10 09:06:01 +0000

    Deploying to gh-pages from @ Liudmyla537/goit-js-hw-11@fced5364a2ac49ae92cc255e85842a9b9ff39ead 🚀

--- index.js
+++ index.js
@@ -4,19 +4,15 @@ import{a as d,S as f,i as a}from"./assets/vendor-CrlV4O_2.js";(function(){const 
         <img src="${e.webformatURL}" data-src="${e.largeImageURL}" alt="${e.tags}" />
       </a>
       <div class="info">
-        <p>Likes</b>
-            ${e.likes}
-         </p>
-        <p>Views</b>
-            ${e.views}
-         </p>
-        <p>Comments</b>
-            ${e.comments}
-         </p>
-        <p>Downloads</b>
-            ${e.downloads}
-         </p>
+        <p><b>Likes</b>
+        ${e.likes}</p>
+        <p><b>Views</b>
+        ${e.views}</p>
+        <p><b>Comments</b>
+        ${e.comments}</p>
+        <p><b>Downloads</b>
+        ${e.downloads}</p>
       </div>
     </li>
-  `).join("");c.insertAdjacentHTML("beforeend",o),g.refresh()}function L(){c.innerHTML=""}function b(){l.classList.remove("hidden")}function w(){l.classList.add("hidden")}const u=document.querySelector(".form"),S=u.querySelector('input[name="search-text"]');u.addEventListener("submit",async s=>{s.preventDefault();const o=S.value.trim();if(!o){a.warning({message:"Please enter a search query.",position:"topRight"});return}b(),L();try{const{hits:e}=await y(o);e.length===0?a.info({message:"Sorry, there are no images matching your search query. Please try again!",position:"topRight"}):h(e)}catch(e){a.error({message:"Something went wrong. Please try again later.",position:"topRight"}),console.error(e)}finally{w()}});
+  `).join("");c.insertAdjacentHTML("beforeend",o),g.refresh()}function b(){c.innerHTML=""}function L(){l.classList.remove("hidden")}function w(){l.classList.add("hidden")}const u=document.querySelector(".form"),S=u.querySelector('input[name="search-text"]');u.addEventListener("submit",async s=>{s.preventDefault();const o=S.value.trim();if(!o){a.warning({message:"Please enter a search query.",position:"topRight"});return}L(),b();try{const{hits:e}=await y(o);e.length===0?a.info({message:"Sorry, there are no images matching your search query. Please try again!",position:"topRight"}):h(e)}catch(e){a.error({message:"Something went wrong. Please try again later.",position:"topRight"}),console.error(e)}finally{w()}});
 //# sourceMappingURL=index.js.map
